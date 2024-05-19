cd0b50667c6a1b1e159a154432f190d19c62638e
mdfahim-anjum <mdfahim-anjum@uiowa.edu>
2024-05-19 20:24:31 +0000

    pypi library version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='turbolpc',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     author="Fahim Anjum",
     author_email="dr.fahim.anjum@gmail.com",
